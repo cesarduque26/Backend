b739bfe3541658383cbafef7a458b90cb07997d7
Cristopher Herrera <82126489+cristoxdxd@users.noreply.github.com>
2024-03-04 23:13:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var port='3600';
 mongoose.promise=global.Promise;
 mongoose.set("strictQuery",false);
 var app=require('./app');
-mongoose.connect('mongodb://127.0.0.1:27017/hotel')
+mongoose.connect('mongodb+srv://root:12345@servermongodb.8gwwirl.mongodb.net/?retryWrites=true&w=majority&appName=ServerMongoDB/hotel')
 .then(()=>{
     console.log("Conexion a BDD");
     app.listen(port,()=>{
